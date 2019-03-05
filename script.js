64164cb65be47659ba222af7b57d4c21fd0e0150
jechen666666 <43358343+jechen666666@users.noreply.github.com>
2019-03-04 16:07:35 -0800

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
-$(".container").mousemove (function(e){
+$(".container").mousemove (function(e) {
   var moveX = (e.pageX * -1/15);
   var moveY = (e.pageY * -1/15);
-  $(this).css("background-position", moveX + "px " + moveY + "px");
+  $(".container").css("background-position", moveX + "px " + moveY + "px");
 });
+
+$(".chosenOne").hide();
+
+$(".chosenTwo").hide();
+
+$(".cplanetOne").hide();
+
+$(".cplanetTwo").hide();
+
+$(".cplanetThree").hide();
+
+$(".hplanetOne").click (function() {
+    $(".hplanets").hide();
+    $("h1").hide();
+    $(".chosenOne").show();
+    $(".cplanetOne").show();
+    $(".cplanetTwo").show();
+    $(".cplanetThree").show();
+});
+
+$(".hplanetTwo").click (function() {
+    $(".hplanets").hide();
+    $("h1").hide();
+    $(".chosenTwo").show();
+    $(".cplanetOne").show();
+    $(".cplanetTwo").show();
+    $(".cplanetThree").show();
+});
+
+$(".hplanetOne").mouseenter (function() {
+    $(".hplanetOne").css("transform", "translateY(-15px)");
+});
+
+$(".hplanetOne").mouseleave (function() {
+    $(".hplanetOne").css("transform", "translateY(0px)");
+});
+
+$(".hplanetTwo").mouseenter (function() {
+    $(".hplanetTwo").css("transform", "translateY(-15px)");
+});
+
+$(".hplanetTwo").mouseleave (function() {
+    $(".hplanetTwo").css ("transform", "translateY(0px)");
+});
+
+
+
+    
